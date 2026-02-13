ba45ec5aa2297b1467aa5eecf7bcd4b5cd5586b3
GustavoBastos1 <motagustavo039@gmail.com>
2026-02-13 00:05:22 -0300

    Add function to verify user activity status

--- escola-de-musica-benicasa/lanchonete.h
+++ escola-de-musica-benicasa/lanchonete.h
@@ -1,1 +1,13 @@
+#include "headers.h"
 
+
+bool verificaratividade(usuario pessoa){
+    if(pessoa.ativo==1)
+        return true;
+    else
+        return false;
+}
+void bancodedadoslanchonete();
+void menuCadastroProdutos();
+void adicionarCreditosUsuario();
+void consultarEstoque();
