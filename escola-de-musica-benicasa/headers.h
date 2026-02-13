85cb555526ab3aabbf7c58d11056086dd12e2d1c
Gabriel Bispo <svgabriiel@gmail.com>
2026-02-12 23:20:52 -0300

    novas structs
    Added new structs for Produto, Curso, Instrumento, and Emprestimo.

--- escola-de-musica-benicasa/headers.h
+++ escola-de-musica-benicasa/headers.h
@@ -25,4 +25,38 @@ struct Usuario{
         especialidade[0] = '\0';
     }    
 };
+struct Produto {
+    int id;
+    int ativo;
+    char nome[50];
+    float preco
+    int estoque;
+};
+struct Curso {
+    int id;
+    int ativo;
+    char nome[50];
+    int cargaHoraria;
+    int vagas;
+};
+
+struct Instrumento {
+    int id;
+    int ativo;          // 1 = ativo, 0 = removido
+    int autorizado;     // 0 = não autorizado, 1 = autorizado
+    char nome[30];
+    int disponivel;     // 1 = disponível, 0 = indisponível
+};
+
+struct Emprestimo {
+    int idEmprestimo;        // Identificador único do empréstimo
+    
+    int idAluno;             // ID do aluno que realizou o empréstimo
+    int idInstrumento;       // ID do instrumento emprestado
+
+    char dataEmprestimo[11]; // Data do empréstimo (dd/mm/aaaa)
+    char dataPrevista[11];   // Data prevista para devolução
+
+};
+
 #endif
